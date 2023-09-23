722142ff059202490bbaf11e172e83e1ce8ec91e
Rivares <ura1994nikolaevic@gmail.com>
2023-09-23 18:31:50 +0300

    Impl  general part of work

--- lib.hpp
+++ lib.hpp
@@ -13,12 +13,37 @@
 #include <set>
 #include <map>
 
+
+//1. Аллокатор должен освобождать всю память самостоятельно.
+//2. Аллокатор работает с фиксированным количеством элементов. Попытку выделить большее число
+//элементов считать ошибкой.
+
+//Опционально:
+//1. Реализовать расширяемость аллокатора. При попытке выделить число элементов,
+//которое превышает текущее зарезервированное количество, аллокатор расширяет
+//зарезервированную память.
+
+//2. Реализовать поэлементное освобождение.
+
+//3. Реализовать свой контейнер, который по аналогии с контейнерами stl параметризуется
+//аллокатором. Контейнер должен иметь две возможности - добавить новый элемент и обойти
+//контейнер в одном направлении.
+
+//4. Реализовать совместимость с контейнерами stl – итераторы, вспомогательные
+//методы size, empty и т.д.
+
+
+
 template<typename T>
 class Allocator // stateless
 {
 public:
 
     using value_type = T;
+    using pointer = T*;
+
+    static int currCnt;
+    static constexpr std::size_t poolSize = 11; // 10 + 1
 
     Allocator() = default;
 
@@ -28,28 +53,34 @@ public:
 //    template<class U>
 //    constexpr Allocator(const Allocator <U>&) noexcept {}
 
-    [[nodiscard]] T* allocate(std::size_t cnt)
+    [[nodiscard]] pointer allocate(std::size_t cnt)
     {
         if (cnt > (std::numeric_limits<std::size_t>::max() / sizeof(T)))
         {   throw std::bad_array_new_length();  }
 
-        if (auto ptr = static_cast<T*>( std::malloc(cnt * sizeof(T)) ))
+        if ((currCnt + 1) < poolSize)
         {
-            report(ptr, cnt);
-            return ptr;
+            if (auto ptr = static_cast<T*>( std::malloc(cnt * sizeof(T)) )) // ::operator new(cnt * sizeof(T))
+            {
+                report(ptr, cnt);
+                ++currCnt;
+                return ptr;
+            }
         }
 
         throw std::bad_alloc();
     }
 
-    void deallocate(T* ptr, [[maybe_unused]] std::size_t cnt) noexcept
+    void deallocate(pointer ptr, [[maybe_unused]] std::size_t cnt) noexcept
     {
         report(ptr, cnt, false);
-        std::free(ptr);
+        --currCnt;
+        std::free(ptr); // ::operator delete(p);
     }
 
+
 private:
-    void report(T* ptr, std::size_t cnt, bool action = true) const
+    void report(pointer ptr, std::size_t cnt, bool action = true) const
     {
         std::cout << ((action) ? "Alloc:\t\t" : "Dealloc:\t") << std::hex << std::showbase
                   << reinterpret_cast<void*>(ptr)  << std::dec << " | "
@@ -58,7 +89,8 @@ private:
 
 };
 
-
+template <typename T>
+int Allocator<T>::currCnt = 0;
 
 template<typename T, typename U>
 constexpr bool operator == (const Allocator<T>&, const Allocator<U>&) noexcept
--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
 #include "lib.hpp"
 
-int main([[maybe_unused]] int argc,[[maybe_unused]] char *argv[])
+[[nodiscard]] int factorial (const int& num)
 {
-    std::vector<int, Allocator<int>> test_0(1);
+    if (num == 0)
+    {   return 1;   }
+
+    int factorial = 1;
+
+    for (size_t i = 1; i <= num; ++i)
+    {   factorial *= i;   }
+
+    return factorial;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+    uint8_t cnt = 10;
+
+    //    - создание экземпляра std::map<int, int>
+    std::map<int, int> test_0;
+
+    //    - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    while (cnt-- > 0)
+    {
+        test_0[cnt] = factorial(cnt);
+    }
+
+    //    - вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
+    for (const auto& item : test_0)
+    {   std::cout << item.first << " " << item.second << "\n";  }
+
+
+
+    //    - создание экземпляра std::map<int, int>
+    std::map<int, int, std::less<int>, Allocator<std::pair<const int, int> >> test_1;
+
+    //    - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    cnt = 10;
+    while (cnt-- > 0)
+    {
+        test_1[cnt] = factorial(cnt);
+    }
+
+    //    - вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
+    for (const auto& item : test_1)
+    {   std::cout << item.first << " " << item.second << "\n";  }
+
+
+
+
+
+    //    - создание экземпляра своего контейнера для хранения значений типа int
+    //    - заполнение 10 элементами от 0 до 9
+    //    - создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
+    //    ограниченным 10 элементами
+    //    - заполнение 10 элементами от 0 до 9
+    //    - вывод на экран всех значений, хранящихся в контейнере
 
     return 0;
 }
